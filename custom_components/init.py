97ca77b1878bd6c45a50fe11bd3c6a5a975a3c67
Total Boss <51457781+totalboss3@users.noreply.github.com>
2025-04-06 17:10:16 -0400

    Update init.py

--- custom_components/init.py
+++ custom_components/init.py
@@ -6,11 +6,9 @@ _LOGGER = logging.getLogger(__name__)
 
 def setup(hass, config):
     """Set up the integration."""
-    conf = config.get('sony_str_dn840')
-    if conf is None:
-        _LOGGER.error("No configuration found for sony_str_dn840")
+    if "sony_str_dn840" not in config:
+        _LOGGER.error("No configuration for sony_str_dn840 found")
         return False
 
-    hass.data.setdefault('sony_str_dn840', {})
-    integration.setup(hass, conf)
-    return True
+    hass.data.setdefault("sony_str_dn840", {})
+    return integration.setup(hass, config)
